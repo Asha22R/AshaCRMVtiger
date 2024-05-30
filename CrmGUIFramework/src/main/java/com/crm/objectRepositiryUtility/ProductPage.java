4e57d2e0e14d98f6de1942d9d2875def4c18d7ad
Asha R <Asha R@FFE-Asha-R.mshome.net>
2024-05-30 14:51:30 +0530

    added addproduct button in product page

--- CrmGUIFramework/src/main/java/com/crm/objectRepositiryUtility/ProductPage.java
+++ CrmGUIFramework/src/main/java/com/crm/objectRepositiryUtility/ProductPage.java
@@ -1,5 +1,20 @@
 package com.crm.objectRepositiryUtility;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ProductPage {
+	WebDriver driver;
+	public ProductPage(WebDriver driver) {
+		this.driver= driver;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath = "//img[@alt=\"Create Product...\"]") private WebElement addProductBtn;
+	public WebElement getAddProductBtn() {
+		return addProductBtn;
+	}
 
 }
